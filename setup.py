dbe6bc291a05c65be3868bec09ed98d9835dd6ca
mayukhdeb <mayukhmainak2000@gmail.com>
2020-09-01 12:57:07 +0530

    setup: initialize setup

---
+++ setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="torch-dreams",
+    version="0.0.1",
+    author="Mayukh Deb", 
+    author_email="mayukhmainak2000@gmail.com", 
+    description="Generate deep-dreams in PyTorch",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Mayukhdeb/torch-dreams",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+
+    install_requires=[
+        "torch>=1.6.0",
+        "torchvision",
+        "opencv-python",
+        "numpy",
+        "matplotlib",
+        "tqdm"
+      ],
+    python_requires='>=3.6',   
+    include_package_data=True
+)
