31797708b396059ee7ce179a98a96851f9493494
mayukhdeb <mayukhmainak2000@gmail.com>
2020-11-14 13:39:08 +0530

    utils: overhaul pre and postprocess

--- torch_dreams/utils.py
+++ torch_dreams/utils.py
@@ -8,56 +8,62 @@ from torchvision import transforms
 from tqdm import tqdm 
 import cv2 
 
-def preprocess_func_vgg(image_np):
+from .constants import IMAGENET_MEAN_1
+from .constants import IMAGENET_STD_1
 
-    """
-    specific for VGG19
-    """
-
-    mean = np.array([0.485, 0.456, 0.406])
-    std = np.array([0.229, 0.224, 0.225])
-    preprocess = transforms.Compose([
-                                     transforms.ToTensor(),
-                                     transforms.Normalize(mean, std)
-                                     ])
-    return preprocess(image_np)
-
-def preprocess_func(image_np):
-
-    """
-    simple version
-    """
-
-    mean = np.array([0.5, 0.5, 0.5])
-    std = np.array([0.5, 0.5, 0.5])
-    preprocess = transforms.Compose([
-                                     transforms.ToTensor()
-                                    #  transforms.Normalize(mean, std)
-                                     ])
-    return preprocess(image_np)
+transform_to_tensor = transforms.Compose([
+                transforms.ToTensor()
+            ])
 
 
-def deprocess_func_vgg(image_tensor):
+def load_image(img_path, target_shape=None):
+    if not os.path.exists(img_path):
+        raise Exception(f'Path does not exist: {img_path}')
+    img = cv2.imread(img_path)[:, :, ::-1]  # [:, :, ::-1] converts BGR (opencv format...) into RGB
 
-    """
-    specific for VGG19
-    """
+    if target_shape is not None:  # resize section
+        if isinstance(target_shape, int) and target_shape != -1:  # scalar -> implicitly setting the width
+            current_height, current_width = img.shape[:2]
+            new_width = target_shape
+            new_height = int(current_height * (new_width / current_width))
+            img = cv2.resize(img, (new_width, new_height), interpolation=cv2.INTER_CUBIC)
+        else:  # set both dimensions to target shape
+            img = cv2.resize(img, (target_shape[1], target_shape[0]), interpolation=cv2.INTER_CUBIC)
 
-    denorm = transforms.Compose([ 
-                                 transforms.Normalize(mean = [ 0., 0., 0. ], std = [ 1/0.229, 1/0.224, 1/0.225 ]),
-                                 transforms.Normalize(mean = [ -0.485, -0.456, -0.406 ], std = [ 1., 1., 1. ]),                                                     
-                                ])
-    return denorm(image_tensor)
+    # this need to go after resizing - otherwise cv.resize will push values outside of [0,1] range
+    img = img.astype(np.float32)  # convert from uint8 to float32
+    img /= 255.0  # get to [0, 1] range
+    return img
 
-def deprocess_func(image_tensor):
+def pytorch_input_adapter(img, device):
+    tensor = transforms.ToTensor()(img).to(device)
+    return tensor
 
 
-    denorm = transforms.Compose([ 
-                                 transforms.Normalize(mean = [ 0., 0., 0. ], std = [ 1/0.5, 1/0.5, 1/0.5 ]),
-                                 transforms.Normalize(mean = [ -0.5, -0.5, -0.5 ], std = [ 1., 1., 1. ]),                                                     
-                                ])
-    return denorm(image_tensor)
+def pytorch_output_adapter(img):
+    return np.moveaxis(img.to('cpu').detach().numpy()[0], 0, 2)
 
+
+def preprocess_numpy_img(img):
+    assert isinstance(img, np.ndarray), f'Expected numpy image got {type(img)}'
+
+    img = (img - IMAGENET_MEAN_1) / IMAGENET_STD_1  # normalize image
+    return img
+
+def post_process_numpy_image(dump_img):
+    assert isinstance(dump_img, np.ndarray), f'Expected numpy image got {type(dump_img)}'
+
+    if dump_img.shape[0] == 3:  # if channel-first format move to channel-last (CHW -> HWC)
+        dump_img = np.moveaxis(dump_img, 0, 2)
+
+    mean = IMAGENET_MEAN_1.reshape(1, 1, -1)
+    std = IMAGENET_STD_1.reshape(1, 1, -1)
+    dump_img = (dump_img * std) + mean  # de-normalize
+    dump_img = np.clip(dump_img, 0., 1.)
+
+    return dump_img
+
+    
 def find_random_roll_values_for_tensor(image_tensor):
 
     """
