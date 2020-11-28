2138966b80735793d65d0e5fe5546a03e38ce2e4
mayukhdeb <mayukhmainak2000@gmail.com>
2020-11-28 11:05:44 +0530

    add grayscale tests

---
+++ test_grayscale.py
@@ -0,0 +1,37 @@
+import torch
+import torch.nn as nn
+import matplotlib.pyplot as plt
+
+from torch_dreams.dreamer import dreamer
+import torchvision.models as models
+
+model = models.resnet18(pretrained=False)
+model.conv1 = nn.Conv2d(1, 64, kernel_size=(7, 7), stride=(2, 2), padding=(3, 3), bias=False)
+model.fc = nn.Linear(in_features=512, out_features=10, bias=True)
+
+model.load_state_dict(torch.load("models/resnet18_mnist.pth"))
+dreamy_boi = dreamer(model)
+
+layers_to_use = [
+                 model.layer4[0].conv2
+            ]
+
+def my_custom_func(layer_outputs):
+    # loss = layer_outputs[0][6]   ## 7th label on FC
+    loss = layer_outputs[0].norm()  
+
+    return loss
+
+out_single_layer = dreamy_boi.deep_dream(
+    image_path = "images/noise.jpg",
+    layers = layers_to_use,
+    octave_scale = 1.2,
+    num_octaves = 14,
+    iterations = 200,
+    lr = 0.9,
+    custom_func = my_custom_func,
+    grayscale = True  ## make sure your model has single channel input
+)
+
+plt.imshow(out_single_layer)
+plt.show()
