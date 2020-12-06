f72fe7c26e30d659c052ab5a72050ca0c3589baa
mayukhdeb <mayukhmainak2000@gmail.com>
2020-12-06 19:30:07 +0530

    test: run on 2 mask sets

--- test_gradient_masks.py
+++ test_gradient_masks.py
@@ -12,7 +12,7 @@ dreamy_boi = dreamer(model)
 
 layers_to_use = [model.Mixed_6c.branch1x1]
 
-grad_mask = np.repeat(np.linspace(0, 1, 512),512*3).reshape(512,512,3).astype(np.float32) 
+grad_mask = np.repeat(np.linspace(0, 1, 512),512*3).reshape(512,512,3).astype(np.float32)**2
 grad_mask_2 = np.repeat(np.linspace(1, 0, 512),512*3).reshape(512,512,3).astype(np.float32) 
 
 def custom_func(layer_outputs):
@@ -35,23 +35,24 @@ config = {
     "custom_func": [custom_func],
     "max_rotation": 0.2,
     "grayscale": False,
-    "gradient_smoothing_coeff": 0.5,
-    "gradient_smoothing_kernel_size": 9,
+    "gradient_smoothing_coeff": 0.1,
+    "gradient_smoothing_kernel_size": 3,
     "grad_mask": [grad_mask]
 }
 
-config2, config3 = config, config
+config2, config3 = config.copy(), config.copy()
 config2["grad_mask"] = [grad_mask_2]
 config2["custom_func"] = [None]
 
 config3["custom_func"] = [custom_func, None]
 config3["grad_mask"] = [grad_mask, grad_mask_2]
 
-# out_single_conv_a = dreamy_boi.deep_dream_with_masks(config)
+out_single_conv_a = dreamy_boi.deep_dream_with_masks(config)
 # out_single_conv_b = dreamy_boi.deep_dream_with_masks(config2)
 out_single_conv = dreamy_boi.deep_dream_with_masks(config3)
 
-
+plt.imshow(out_single_conv_a)
+plt.show()
 plt.imshow(out_single_conv)
 plt.show()
 
