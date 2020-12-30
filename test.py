74a92d682e5e3b8599d59384ae441113433299f1
mayukhdeb <mayukhmainak2000@gmail.com>
2020-12-31 01:58:35 +0530

    test: add tests for input as np.array

--- test.py
+++ test.py
@@ -4,6 +4,11 @@ import matplotlib.pyplot as plt
 
 from torch_dreams.dreamer import dreamer
 import torchvision.models as models
+from torch_dreams.utils import load_image, preprocess_numpy_img
+
+"""
+needs a cleanup
+"""
 
 model = models.inception_v3(pretrained=True)
 dreamy_boi = dreamer(model, quiet_mode= True)
@@ -35,6 +40,19 @@ plt.imshow(out_single_conv)
 plt.show()
 
 """
+With numpy image
+"""
+
+config["image_path"] = None
+config["image"] =  preprocess_numpy_img(load_image("images/sample_small.jpg"))
+
+out_single_conv = dreamy_boi.deep_dream(config)
+
+plt.imshow(out_single_conv)
+plt.show()
+
+
+"""
 Testing grad masks below
 """
 layers_to_use = [model.Mixed_6c.branch1x1]
@@ -50,9 +68,9 @@ config = {
     "image_path": "images/sample_small.jpg",
     "layers": layers_to_use,
     "octave_scale": 1.1,
-    "num_octaves": 11,
+    "num_octaves": 3,
     "iterations": 20,
-    "lr": 0.03,
+    "lr": 0.07,
     "custom_func": [custom_func],
     "max_rotation": 0.5,
     "grayscale": False,
@@ -67,6 +85,22 @@ config2["custom_func"] = [None]
 
 config3["custom_func"] = [custom_func, None]
 config3["grad_mask"] = [grad_mask, grad_mask_2]
+
+out_single_conv_a = dreamy_boi.deep_dream_with_masks(config)
+# out_single_conv_b = dreamy_boi.deep_dream_with_masks(config2)
+out_single_conv = dreamy_boi.deep_dream_with_masks(config3)
+
+plt.imshow(out_single_conv_a)
+plt.show()
+plt.imshow(out_single_conv)
+plt.show()
+
+"""
+with numpy image
+"""
+
+config["image_path"] = None
+config["image"] =  preprocess_numpy_img(load_image("images/sample_small.jpg"))
 
 out_single_conv_a = dreamy_boi.deep_dream_with_masks(config)
 # out_single_conv_b = dreamy_boi.deep_dream_with_masks(config2)
