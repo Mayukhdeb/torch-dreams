462a7c4de9ad3a03ddc7ce85bcbd1670164bf140
mayukhdeb <mayukhmainak2000@gmail.com>
2021-03-16 22:47:31 +0530

    torch_dreams: auto: offload utils

--- torch_dreams/auto/dreamer.py
+++ torch_dreams/auto/dreamer.py
@@ -15,26 +15,17 @@ from .utils import (
 )
 
 from .transforms import random_resize
+from .auto_image_param import auto_image_param
 
 class dreamer():
-    def __init__(self, model, progress = True, device = 'cuda'):
+    def __init__(self, model, quiet = True, device = 'cuda'):
         self.model = model 
         self.model.eval()
         self.device = device
         self.model.to(self.device)
         self.default_func = default_func_mean
         self.transforms = None
-
-    def get_image_param(self, height, width, sd, device):
-        image_parameter = init_image_param(height = height, width = width, sd = 0.01, device = device)
-        return image_parameter
-
-    def get_optimizer(self, params_list, optimizer = None, lr = 1e-3, weight_decay = 0.):
-        if optimizer is not None:
-            optimizer = optimizer(params_list, lr = lr, weight_decay = weight_decay)
-        else:
-            optimizer = torch.optim.AdamW(params_list, lr=lr, weight_decay=weight_decay)
-        return optimizer
+        self.quiet = quiet
 
     def get_default_transforms(self, rotate, scale_max, scale_min):
         self.transforms= transforms.Compose([
@@ -47,44 +38,35 @@ class dreamer():
 
     def render(self, width, height, iters, layers, lr, rotate_degrees, scale_max = 1.1,  scale_min = 0.5, custom_func = None, weight_decay = 0., grad_clip = 1.):
 
-        image_parameter = self.get_image_param(height = height, width = width, sd = 0.01, device = self.device)
-        image_parameter.requires_grad_()
-
-        self.optimizer = self.get_optimizer(
-            params_list = [image_parameter],
-            lr = lr,
-            weight_decay= weight_decay
-        )
-
+        image_parameter = auto_image_param(height= height, width = width, device = self.device, standard_deviation = 0.01)
+        image_parameter.get_optimizer(lr = lr, weight_decay = weight_decay)
         if self.transforms is None:
             self.get_default_transforms(rotate = rotate_degrees, scale_max = scale_max, scale_min= scale_min)
         else:
             print("using your custom transforms")
-
 
         hooks = []
         for layer in layers:
             hook = Hook(layer)
             hooks.append(hook)
 
-        for i in tqdm(range(iters)):
-            self.optimizer.zero_grad()
+        for i in tqdm(range(iters), disable= self.quiet):
+            image_parameter.optimizer.zero_grad()
 
-            img = fft_to_rgb(height, width, image_parameter)
+            img = fft_to_rgb(height, width, image_parameter.param)
             img = lucid_colorspace_to_rgb(img)
             img = torch.sigmoid(img)
             img = normalize(img)
             img = self.transforms(img)
 
-            if i % 100 ==0:
-                import matplotlib.pyplot as plt
+            # if i % 100 ==0:
+            #     import matplotlib.pyplot as plt
 
-                foo = img.detach()[0].cpu().permute(1,2,0)
-                plt.imshow(foo)
-                plt.show()
+            #     foo = img.detach()[0].cpu().permute(1,2,0)
+            #     plt.imshow(foo)
+            #     plt.show()
 
             model_out = self.model(img)
-
 
             layer_outputs = []
 
@@ -97,14 +79,14 @@ class dreamer():
             else:
                 loss = self.default_func(layer_outputs)
             loss.backward()
-            torch.nn.utils.clip_grad_norm_(image_parameter,grad_clip)
-            self.optimizer.step()
+            image_parameter.clip_grads(grad_clip= 1)
+            image_parameter.optimizer.step()
         
 
         for hook in hooks:
             hook.close()
 
-        return image_buf_to_rgb(height, width, image_parameter)
+        return image_parameter
 
 
 # def visualize_feature(model, layer, feature, start_image=None, last_hook_out=None,
