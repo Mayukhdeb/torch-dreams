6fde74eef16f4af2ef48fa553b38b5afc955161a
mayukhdeb <mayukhmainak2000@gmail.com>
2021-03-16 23:14:17 +0530

    torch_dreams: auto: add docstrings

--- torch_dreams/auto/dreamer.py
+++ torch_dreams/auto/dreamer.py
@@ -14,7 +14,15 @@ from .transforms import random_resize
 from .auto_image_param import auto_image_param
 
 class dreamer():
+    """wrapper over a pytorch model for visualization
+
+        Args:
+            model (torch.nn.Module): pytorch model 
+            quiet (bool, optional): enable or disable progress bar. Defaults to True.
+            device (str, optional): 'cpu' or 'cuda'. Defaults to 'cuda'.
+    """
     def __init__(self, model, quiet = True, device = 'cuda'):
+        
         self.model = model 
         self.model.eval()
         self.device = device
@@ -33,6 +41,24 @@ class dreamer():
         self.transforms = transforms
 
     def render(self, width, height, iters, layers, lr, rotate_degrees, scale_max = 1.1,  scale_min = 0.5, custom_func = None, weight_decay = 0., grad_clip = 1.):
+        """core function to visualize elements form within the pytorch model
+
+        Args:
+            width (int): width of image to be optimized 
+            height (int): height of image to be optimized 
+            iters (int): number of iterations, higher -> stronger visualization
+            layers (iterable): [model.layer1, model.layer2...]
+            lr (float): learning rate
+            rotate_degrees (int): max rotation in default transforms
+            scale_max (float, optional): Max image size factor. Defaults to 1.1.
+            scale_min (float, optional): Minimum image size factor. Defaults to 0.5.
+            custom_func (function, optional): See docs for a better explanation. Defaults to None.
+            weight_decay (float, optional): Weight decay for default optimizer. Helps prevent high frequency noise. Defaults to 0.
+            grad_clip (float, optional): Maximum value of norm of gradient. Defaults to 1.
+
+        Returns:
+            image_parameter instance: To show image, use: plt.imshow(image_parameter.rgb)
+        """
 
         image_parameter = auto_image_param(height= height, width = width, device = self.device, standard_deviation = 0.01)
         image_parameter.get_optimizer(lr = lr, weight_decay = weight_decay)
