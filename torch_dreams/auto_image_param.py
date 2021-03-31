c80de0f88080659b3a98cdf3c7b458bb2e38ca25
mayukhdeb <mayukhmainak2000@gmail.com>
2021-03-31 11:59:19 +0530

    torch_dreams: add BaseImageParam

--- torch_dreams/auto_image_param.py
+++ torch_dreams/auto_image_param.py
@@ -5,14 +5,13 @@ import numpy as np
 from .utils import init_image_param, image_buf_to_rgb
 import torchvision.transforms as transforms
 
-class auto_image_param():
-    def __init__(self, height, width, device, standard_deviation):
-        self.height = height
-        self.width = width
-        self.param = init_image_param(height = height, width = width, sd = standard_deviation, device = device)
-        self.param.requires_grad_()
+class BaseImageParam():
+    def __init__(self):
+        self.height = None
+        self.width = None
+        self.param = None
         self.optimizer = None
-
+        
     def fetch_optimizer(self, params_list, optimizer = None, lr = 1e-3, weight_decay = 0.):
         if optimizer is not None:
             optimizer = optimizer(params_list, lr = lr, weight_decay = weight_decay)
@@ -56,3 +55,15 @@ class auto_image_param():
         ten = self.to_chw_tensor()
         im = transforms.ToPILImage()(ten)
         im.save(filename)
+
+
+class auto_image_param(BaseImageParam):
+    def __init__(self, height, width, device, standard_deviation):
+        super().__init__()
+        self.height = height
+        self.width = width
+        self.param = init_image_param(height = height, width = width, sd = standard_deviation, device = device)
+        self.param.requires_grad_()
+        self.optimizer = None
+
+    
