a5604a4b3b23eb75d7b163f08e7432d2a4f1412e
mayukhdeb <mayukhmainak2000@gmail.com>
2021-04-02 18:49:44 +0530

    remove models

--- torch_dreams/__init__.py
+++ torch_dreams/__init__.py
@@ -1,6 +1,5 @@
 from .dreamer import *
 from .utils import *
-from .models import *
 from .model_bunch import *
 from .tests import *
 
@@ -9,7 +8,6 @@ __version__ = "2.0.3"
 __all__ = [
     "dreamer",
     "utils",
-    "models",
     "model_bunch",
     "auto_image_param",
     "image_transforms",
