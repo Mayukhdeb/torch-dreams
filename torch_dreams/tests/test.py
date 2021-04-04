ac9aa2f4dd5018c83dd9c5add5f08f80e58db64d
mayukhdeb <mayukhmainak2000@gmail.com>
2021-04-04 12:37:11 +0530

    torch_dreams: tests: include custom_image_param

--- torch_dreams/tests/test.py
+++ torch_dreams/tests/test.py
@@ -5,6 +5,7 @@ import torchvision.models as models
 from torch_dreams.dreamer import dreamer
 
 from torch_dreams.auto_image_param import auto_image_param
+from torch_dreams.custom_image_param import custom_image_param
 from torch_dreams.model_bunch import ModelBunch
 import numpy as np
 import torch
@@ -120,6 +121,31 @@ class test(unittest.TestCase):
 
         os.remove('test_multiple_models_custom_func.jpg')
 
+    def test_custom_image_param(self):
+
+        model = models.inception_v3(pretrained=True)
+
+        dreamy_boi = dreamer(model = model, device= 'cpu', quiet= False)
+        param = custom_image_param(filename = 'images/sample_small.jpg', device= 'cuda')
+
+        image_param = dreamy_boi.render(
+            image_parameter= param,
+            layers = [model.Mixed_6a],
+            iters = 5,
+            lr = 2e-4,
+            grad_clip = 0.1,
+            weight_decay= 1e-1
+        )
+
+        image_param.save(filename = 'test_custom_image_param.jpg')
+
+        self.assertTrue(os.path.exists('test_custom_image_param.jpg'))
+        self.assertTrue(isinstance(image_param, custom_image_param), 'should be an instance of auto_image_param')
+        self.assertTrue(isinstance(image_param.__array__(), np.ndarray))
+        self.assertTrue(isinstance(image_param.to_hwc_tensor(), torch.Tensor), 'should be a torch.Tensor')
+        self.assertTrue(isinstance(image_param.to_chw_tensor(), torch.Tensor), 'should be a torch.Tensor')
+        os.remove('test_custom_image_param.jpg')
+        
 
 if __name__ == '__main__':
 
