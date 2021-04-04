f18872db423fc1f7a871d543f7f32331738caa0e
mayukhdeb <mayukhmainak2000@gmail.com>
2021-04-04 18:27:38 +0530

    torch_dreams: __init__: update imports

--- torch_dreams/__init__.py
+++ torch_dreams/__init__.py
@@ -2,6 +2,8 @@ from .dreamer import *
 from .utils import *
 from .model_bunch import *
 from .tests import *
+from .auto_image_param import auto_image_param
+from .custom_image_param import custom_image_param
 
 __version__ = "2.0.4"
 
@@ -10,6 +12,7 @@ __all__ = [
     "utils",
     "model_bunch",
     "auto_image_param",
+    "custom_image_param",
     "image_transforms",
     "transforms"
 ]
