e405496d86f519d102fd6ed0fdf513fcf4171581
mayukhdeb <mayukhmainak2000@gmail.com>
2021-06-29 15:11:10 +0530

    Fix #48: formalise support for torch v1.9.x

--- torch_dreams/error_handlers.py
+++ torch_dreams/error_handlers.py
@@ -1,11 +1,11 @@
 class PytorchVersionError(Exception):
-    """Raised when the user is not on pytorch 1.8.x
+    """Raised when the user is not on pytorch 1.8.x or higher
 
     Args:
         version (str): torch.__version__
     """ 
     def __init__(self, version):
         self.version = version
-        self.message = "Expected pytorch to have version 1.8.x but got: " + self.version + "\n Please consider updating pytorch from: https://pytorch.org/get-started/locally"
+        self.message = "Expected pytorch to have version 1.8.x or higher but got: " + self.version + "\n Please consider updating pytorch from: https://pytorch.org/get-started/locally"
     def __str__(self):
         return self.message
--- torch_dreams/utils.py
+++ torch_dreams/utils.py
@@ -71,7 +71,7 @@ def fft_to_rgb(height, width, image_parameter, device = 'cuda'):
     image_parameter = torch.complex(image_parameter[..., 0], image_parameter[..., 1])
     t = scale * image_parameter
    
-    if  torch.__version__[:3] == '1.8':
+    if  torch.__version__[:3] == '1.8' or  torch.__version__[:3] == '1.9':
 
         t = torch.fft.irfft2(t,  s = (height, width), norm = 'ortho')
     else:
@@ -126,7 +126,7 @@ def fft_to_rgb_custom_img(height, width, image_parameter, device = 'cuda'):
     scale = get_fft_scale_custom_img(height, width , device= device).to(image_parameter.device)
     t = scale * image_parameter
    
-    if  torch.__version__[:3] == '1.8':
+    if  torch.__version__[:3] == '1.8' or  torch.__version__[:3] == '1.9':
         t = torch.fft.irfft2(t,  s = (height, width), norm = 'ortho')
     else:
         raise PytorchVersionError(version = torch.__version__)
