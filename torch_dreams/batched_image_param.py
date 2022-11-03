f08b5177ca5e728ec47ef156b1964aba97091a21
mayukhdeb <mayukhmainak2000@gmail.com>
2022-11-03 18:14:25 +0530

    add batched optim + batched auto image param

--- torch_dreams/batched_image_param.py
+++ torch_dreams/batched_image_param.py
@@ -1,6 +1,20 @@
 import torch
 from typing import List
-from .auto_image_param import BaseImageParam
+from .auto_image_param import BaseImageParam, AutoImageParam
+
+
+class BatchedOptimizer:
+    def __init__(self, optimizers: List) -> None:
+        """Thin abstraction layer to make sure the top level stuff does not change in torch_dreams.Dreamer().render()
+
+        Args:
+            optimizers (List): list of torch optimizers
+        """
+        self.optimizers = optimizers
+
+    def zero_grad(self):
+        for opt in self.optimizers:
+            opt.zero_grad()
 
 
 class BatchedImageParam:
@@ -24,3 +38,28 @@ class BatchedImageParam:
             [x.forward(device=device) for x in self.image_params]
         )
         return image_param_batch
+
+
+class BatchedAutoImageParam(BatchedImageParam):
+    def __init__(
+        self,
+        batch_size: int = 1,
+        height: int = 256,
+        width: int = 256,
+        standard_deviation: float = 0.01,
+        device: str = "cuda:0",
+    ) -> None:
+        image_params = [
+            AutoImageParam(
+                height=height,
+                width=width,
+                standard_deviation=standard_deviation,
+                device=device,
+            )
+            for x in range(batch_size)
+        ]
+
+        self.optimizer = BatchedOptimizer(
+            optimizers=[x.optimizer for x in image_params]
+        )
+        super().__init__(image_params)
