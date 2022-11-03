7eef030da2cfcd521b7a2332097cd3f3fb736ab4
mayukhdeb <mayukhmainak2000@gmail.com>
2022-11-04 00:29:09 +0530

    fix bugs + send loss to right device

--- torch_dreams/batched_objective.py
+++ torch_dreams/batched_objective.py
@@ -12,22 +12,24 @@ class BatchedObjective:
         """
         self.objectives = objectives
 
-    def __call__(self, x):
-        assert torch.is_tensor(
-            x
-        ), f"Expected x to be a torch.tensor, but got: {type(x)}"
+    def __call__(self, x: list):
 
-        batch_size = x.shape[0]
+        for y in x:
+            assert torch.is_tensor(
+                y
+            ), f"Expected every item in x to be a torch.tensor, but got: {type(y)}"
+
+        batch_size = x[0].shape[0]
 
         assert batch_size == len(
             self.objectives
         ), f"Batch size of x and len(self.objectives) do not match: {x.shape[0]} and {len(self.objectives)}"
 
-        loss_sum = torch.tensor(0.0)
+        loss_sum = torch.tensor(0.0).to(x[0].device)
 
-        for i in range(batch_size):
-            loss_sum += self.objectives[i](
-                x[i].unsqueeze(0)
+        for batch_idx in range(batch_size):
+            loss_sum += self.objectives[batch_idx](
+                x[0][batch_idx].unsqueeze(0)
             )  ## is unsqueeze this really necessary? idk
 
         return loss_sum  ## then call loss_sum.backward()
