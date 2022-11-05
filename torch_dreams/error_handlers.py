0049ddd1928354be4d45109f1e12a6b6da0ba16a
mayukhdeb <mayukhmainak2000@gmail.com>
2022-11-05 12:08:12 +0530

    apply formatting

--- torch_dreams/error_handlers.py
+++ torch_dreams/error_handlers.py
@@ -3,9 +3,15 @@ class PytorchVersionError(Exception):
 
     Args:
         version (str): torch.__version__
-    """ 
+    """
+
     def __init__(self, version):
         self.version = version
-        self.message = "Expected pytorch to have version 1.8.x or higher but got: " + self.version + "\n Please consider updating pytorch from: https://pytorch.org/get-started/locally"
+        self.message = (
+            "Expected pytorch to have version 1.8.x or higher but got: "
+            + self.version
+            + "\n Please consider updating pytorch from: https://pytorch.org/get-started/locally"
+        )
+
     def __str__(self):
         return self.message
