c60d46256bfe2104fb51115f88e0dcda0d879ffe
mayukhdeb <mayukhmainak2000@gmail.com>
2024-02-25 22:18:20 +0530

    I think we're on the right track now

--- torch_dreams/maco/magnitude_spectrum.py
+++ torch_dreams/maco/magnitude_spectrum.py
@@ -18,7 +18,7 @@ def get_magnitude_spectrum(image: Image, take_log=False):
     # Use torch.fft.fftn for n-dimensional FFT, here applied to 2D (image)
     # The function requires a complex input, but our image is real, so we make it complex
     # with a zero imaginary part using torch.view_as_complex
-    fft_result = torch.fft.fftn(image_tensor, dim=[2, 3])
+    fft_result = torch.fft.fftn(image_tensor, dim=[2, 3], norm = "ortho")
 
     # Compute the power spectrum (magnitude squared of the FFT)
     # abs() computes the magnitude for complex tensors, then we square it
@@ -31,7 +31,7 @@ def get_magnitude_spectrum(image: Image, take_log=False):
     power_spectrum_centered = power_spectrum_centered.squeeze()
 
     if take_log:
-        power_spectrum_centered = torch.log(1 + power_spectrum_centered)
+        power_spectrum_centered = torch.log(power_spectrum_centered)
 
     return power_spectrum_centered
 
